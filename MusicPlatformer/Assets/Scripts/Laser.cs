1521f64240eb8490f74b3938a36cd367baac4f55
Dylan <dytu4626@colorado.edu>
2019-12-04 14:42:09 -0700

    more changes, incontrol

---
+++ MusicPlatformer/Assets/Scripts/Laser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    LineRenderer laser;
+    float laserWidth = 0.1f;
+    float laserMaxLength = 20f;
+
+    RaycastHit2D hit;
+    public LayerMask canHit;
+    public Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+        Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
+        laser.SetPositions(initLaserPositions);
+        laser.startWidth = laserWidth;
+        laser.endWidth = laserWidth;
+        laser.enabled = true;
+        laser.startColor = Color.red;
+        laser.endColor = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, canHit);
+        Vector3 endPosition = transform.position + (laserMaxLength * direction);
+        endPosition = hit.point;
+        laser.SetPosition(0, transform.position);
+        laser.SetPosition(1, endPosition);
+    }
+}
