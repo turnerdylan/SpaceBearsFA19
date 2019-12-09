25d67995a76af08045e6ad6f110a7435fb2bd553
Damian <daho2353@colorado.edu>
2019-12-09 13:13:32 -0700

    Sound effects

--- MusicPlatformer/Assets/Scripts/Level-Related/Respawner.cs
+++ MusicPlatformer/Assets/Scripts/Level-Related/Respawner.cs
@@ -7,12 +7,15 @@ public class Respawner : MonoBehaviour
     public Transform otherSpawn;
     public Transform bearSpawn;
 
+    public AudioSource Death;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var lay = collision.gameObject.layer;
         if (lay == 8 || lay == 9 || lay == 10)
         {
             collision.gameObject.transform.position = bearSpawn.position;
+            Death.Play();
             return;
         }
         collision.gameObject.transform.position = otherSpawn.position;
